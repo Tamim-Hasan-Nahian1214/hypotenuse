ace6b910befd5496354180dfa30e8e03f756d590
Tamim-Hasan-Nahian1214 <261452465+Tamim-Hasan-Nahian1214@users.noreply.github.com>
2026-03-30 10:30:22 +0600

    adding more functionalities in the main.cpp file

--- Hypotenuse side Calculator/main.cpp
+++ Hypotenuse side Calculator/main.cpp
@@ -12,6 +12,42 @@ double addition() {
     std::cout << "The Addition is: " << additionResult << std::endl;
 }
 
+double subtraction()
+{
+    int subtractionA;
+    std::cout << "Enter Number 1: ";
+    std::cin >> subtractionA;
+    int subtractionB;
+    std::cout << "Enter Number 2: ";
+    std::cin >> subtractionB;
+    int subtractionResult = subtractionA - subtractionB;
+    std::cout << "The Subtraction is: " << subtractionResult << std::endl;
+}
+
+double multiplication()
+{
+    int multiplicationA;
+    std::cout << "Enter Number 1: ";
+    std::cin >> multiplicationA;
+    int multiplicationB;
+    std::cout << "Enter Number 2: ";
+    std::cin >> multiplicationB;
+    int multiplicationResult = multiplicationA * multiplicationB;
+    std::cout << "The Multiplication is: " << multiplicationResult << std::endl;
+}
+
+double division()
+{
+    int divisionA;
+    std::cout << "Enter Number 1: ";
+    std::cin >> divisionA;
+    int divisionB;
+    std::cout << "Enter Number 2: ";
+    std::cin >> divisionB;
+    int divisionResult = divisionA / divisionB;
+    std::cout << "The Division is: " << divisionResult << std::endl;
+}
+
 double hypotenuse()
 {
     int hypotenuseA;
@@ -30,5 +66,8 @@ double hypotenuse()
 {
     hypotenuse();
     addition();
+    subtraction();
+    multiplication();
+    division(); 
     return 0;
 }
--- Hypotenuse side Calculator/tempCodeRunnerFile.cpp
+++ Hypotenuse side Calculator/tempCodeRunnerFile.cpp
@@ -1,14 +1,3 @@
-#include <iostream> 
-#include <cmath>
-int main() {
-    int a;
-    std::cout << "Enter the size of a: ";
-    std::cin >> a; 
-    int b;
-    std::cout << "Enter the size of b: ";
-    std::cin >> b; 
-    double c;
-    c = std::sqrt(std::pow(a, 2) + std::pow(b, 2));
-    std::cout << c << std::endl;
-    return 0;
-}
+
+    // addition();
+    // subtraction();
